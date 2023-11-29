91586689d0a76d8e18379dbb2503f8a9bb235067
lnotspotl <lnotspotl@gmail.com>
2023-11-29 23:41:19 +0100

    add tests

---
+++ tests/test_packet.py
@@ -0,0 +1,16 @@
+from udpai.packet import Packet, PacketType
+
+def test_serialize():
+    packet = Packet(PacketType.DATA, 0, b'Lorem ipsum')
+    serialized = packet.to_bytes()
+    packet_reconstructed = Packet.from_bytes(serialized)
+
+    assert packet.type == packet_reconstructed.type
+    assert packet.crc == packet_reconstructed.crc
+    assert packet.data_len == packet_reconstructed.data_len
+    assert packet.data == packet_reconstructed.data
+    assert packet.check_crc()
+
+def test_packet_crc():
+    packet = Packet(PacketType.DATA, 0, b'hello world')
+    assert packet.check_crc()
