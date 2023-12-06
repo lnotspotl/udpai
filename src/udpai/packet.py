d2b4dcaa31cddffb4d2bdde7eae3c53bc2f13fee
lnotspotl <lnotspotl@gmail.com>
2023-12-06 17:30:53 +0100

    add packet id

--- src/udpai/packet.py
+++ src/udpai/packet.py
@@ -9,6 +9,7 @@ from hashlib import sha1
 PACKET_TYPE_SIZE = 1  # 1 byte
 CRC_SIZE = 4  # 4 bytes
 DATA_LEN_SIZE = 4  # 4 bytes
+PACKET_ID_SIZE = 4  # 4 bytes
 HASH_SIZE = len(sha1(b"Hello world").hexdigest().encode('utf-8'))
 DATA_SIZE = 1024  # 1 KB
 ENDIAN = 'big'
@@ -20,11 +21,12 @@ class PacketType(Enum):
     ACK = 3
 
 class Packet:
-    def __init__(self, type: PacketType, data_len: int, data: bytes, crc=None, hash=None):
+    def __init__(self, type: PacketType, data_len: int, data: bytes, packet_id: int, crc=None, hash=None):
         self.type = type
         self.crc = crc
         self.hash = hash
         self.data_len = data_len
+        self.packet_id = packet_id
         self.data = data
 
         if hash is None:
@@ -56,11 +58,12 @@ class Packet:
 
     def _calculate_crc(self):
         assert self.hash is not None
-        crc_bytes = self.type_bytes + self.data_len_bytes + self.data_bytes + self.hash_bytes
+        crc_bytes = self.type_bytes + self.data_len_bytes + self.data_bytes + self.hash_bytes + self.packet_id_bytes
         return crc32(crc_bytes)
 
     def to_bytes(self):
-        return self.type_bytes + self.crc_bytes + self.data_len_bytes + self.hash_bytes + self.data_bytes
+        assert self.hash is not None
+        return self.type_bytes + self.crc_bytes + self.data_len_bytes + self.hash_bytes + self.packet_id_bytes + self.data_bytes
     
     @classmethod
     def from_bytes(cls, packet_bytes):
@@ -69,8 +72,9 @@ class Packet:
         data_len = cls._data_len_from_bytes(packet_bytes)
         data = cls._data_from_bytes(packet_bytes)
         hash = cls._hash_from_bytes(packet_bytes)
+        packet_id = cls._packet_id_from_bytes(packet_bytes)
 
-        return cls(type, data_len, data, crc, hash)
+        return cls(type, data_len, data, packet_id, crc, hash)
     
     @property
     def type_bytes(self):
@@ -91,6 +95,10 @@ class Packet:
     @property
     def hash_bytes(self):
         return self.hash
+    
+    @property
+    def packet_id_bytes(self):
+        return self.packet_id.to_bytes(PACKET_ID_SIZE, byteorder=ENDIAN)
 
     @staticmethod
     def _type_from_bytes(packet_bytes):
@@ -112,12 +120,19 @@ class Packet:
     
     @staticmethod
     def _hash_from_bytes(packet_bytes):
-        start, stop = PACKET_TYPE_SIZE + CRC_SIZE + DATA_LEN_SIZE, PACKET_TYPE_SIZE + CRC_SIZE + DATA_LEN_SIZE + HASH_SIZE
+        start = PACKET_TYPE_SIZE + CRC_SIZE + DATA_LEN_SIZE
+        stop = start + HASH_SIZE
         hash = packet_bytes[start:stop]
         return hash
     
     @staticmethod
-    def _data_from_bytes(packet_bytes):
+    def _packet_id_from_bytes(packet_bytes):
         start = PACKET_TYPE_SIZE + CRC_SIZE + DATA_LEN_SIZE + HASH_SIZE
+        stop = start + PACKET_ID_SIZE
+        return int.from_bytes(packet_bytes[start:stop], byteorder=ENDIAN)
+    
+    @staticmethod
+    def _data_from_bytes(packet_bytes):
+        start = PACKET_TYPE_SIZE + CRC_SIZE + DATA_LEN_SIZE + HASH_SIZE  + PACKET_ID_SIZE
         data = packet_bytes[start:]
         return data
